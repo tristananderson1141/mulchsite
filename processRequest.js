866fb13e1ebb0b0d0cbf52858006922505b61c93
tristananderson1141 <muttonjones72@outlook.com>
2021-01-04 17:42:06 -0500

    added mulch calculator and nav bar

--- processRequest.js
+++ processRequest.js
@@ -1,1 +1,26 @@
-alert("Connection Established");
+function openCalcPage() {
+    window.open("Mulch-Area-Calculator.html", "_self");
+}
+
+function openHomePage() {
+    window.open("Mulch-Gravel-Site.html","_self");
+}
+
+function calcArea() {
+
+    var length = document.getElementById("length").value;
+    var width = document.getElementById("width").value;
+    var depth = document.getElementById("depth").value;
+    depth = depth / 12;
+
+    var area = length * width * depth;
+
+    area = area / 3;
+
+    var result = area + " yd^3"
+
+    document.getElementById("result").textContent = result;
+}
+
+document.getElementById("mulchCalc").addEventListener("click", openCalcPage);
+document.getElementById("homePage").addEventListener("click", openHomePage);
